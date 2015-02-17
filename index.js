2f9484c20717d9d7fc3b85f548302f3daf464238
FruitieX <fruitiex@gmail.com>
2015-02-18 01:36:46 +0200

    add support for http auth

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var tlsOpts = {
     rejectUnauthorized: config.rejectUnauthorized
 };
 
-var socket = require('socket.io-client')(config.hostname + ':' + config.port, tlsOpts);
+var socket = require('socket.io-client')((config.tls ? 'https://' : 'http://') + config.hostname + ':' + config.port, tlsOpts);
 var spawn = null;
 
 socket.on('playback', function(data) {
@@ -28,28 +28,33 @@ socket.on('playback', function(data) {
     if(data.position)
         seek = data.position / 1000;
 
-    var args = [];
+    var args = ['-ss', seek, '-nodisp', '-nostats']; //'-v', 'debug',
+
     if(config.tls) {
-        args = ['-ss', seek, '-nodisp', '-nostats',
+        args.splice(args.length, 0,
+            '-ss', seek, '-nodisp', '-nostats',
             '-cert_file', config.tlsCert,
             '-ca_file', config.tlsCa,
             '-key_file', config.tlsKey,
-            '-tls_verify', (config.rejectUnauthorized ? 1 : 0),
-            config.hostname + ':' + config.port + '/song/' + data.backendName + '/' + data.songID + '.' + data.format
-        ]
-    } else {
-        args = ['-ss', seek, '-nodisp', '-nostats',
-            //'-v', 'debug',
-            config.hostname + ':' + config.port + '/song/' + data.backendName + '/' + data.songID + '.' + data.format
-        ]
+            '-tls_verify', (config.rejectUnauthorized ? 1 : 0)
+        );
     }
+
+    // url
+    args.push(config.tls ? 'https://' : 'http://');
+    if(config.plugins.indexOf('httpAuth') !== -1) {
+        args[args.length - 1] += config.username + ':' + config.password + '@';
+    }
+
+    args[args.length - 1] += (config.hostname + ':' + config.port + '/song/' + data.backendName + '/' + data.songID + '.' + data.format);
 
     spawn = Spawn({
         cmd: 'ffplay',
-        args: args
-        //onStderr: function() {}
+        args: args,
+        onStderr: function() {}
     });
     spawn.start();
+
     console.log('playing: ' + config.hostname + ':' + config.port + '/song/' + data.backendName + '/' + data.songID + '.' + data.format);
 });
 
